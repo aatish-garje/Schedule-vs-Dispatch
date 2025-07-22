62c61ef683319bff2d3bf4911ba2beb3e592f9ba
Aatish Garje <103564805+aatish-garje@users.noreply.github.com>
2025-07-22 10:44:13 +0530

    Add files via upload

--- fg.py
+++ fg.py
@@ -6,7 +6,7 @@ from openpyxl.styles import Font, Border, Side, Alignment
 from openpyxl import Workbook
 
 # Material group codes as per your specification
-power_codes = ['80339', '80349', '80439', '80469', '80489', '80499', '88439', 'M0339', 'M0439']
+power_codes = ['80339', '80379' '80349', '80439', '80469', '80489', '80499', '88439', 'M0339', 'M0439']
 vane_pump_codes = ['76139', '76729', '76739', '76749', '76769', '76919']
 mechanical_codes = ['73409', '78209']
 bevel_gear_codes = ['78609']
@@ -16,9 +16,11 @@ oil_tank_codes = ['7632472', '7672472', '7632975501']
 def matches_any(x, codes):
     return any(str(x).startswith(code) for code in codes)
 
-# Function to remove only 7613955137/99
 def not_761395513799_and_513899(x):
     return str(x) not in ["7613955137/99", "7613955138/99"]
+
+def drop_arm_no_slash(x):
+    return '/' not in str(x)
 
 def add_subtotal(df):
     df = df.copy()
@@ -96,7 +98,12 @@ if uploaded_file:
         ),
         "Mechanical": add_subtotal(df[df['Material'].apply(lambda x: matches_any(x, mechanical_codes))][col7]),
         "Bevel Gear": add_subtotal(df[df['Material'].apply(lambda x: matches_any(x, bevel_gear_codes))][col7]),
-        "Drop Arm": add_subtotal(df[df['Material'].apply(lambda x: matches_any(x, drop_arm_codes))][col7]),
+        "Drop Arm": add_subtotal(
+            df[
+                df['Material'].apply(lambda x: matches_any(x, drop_arm_codes))
+                & df['Material'].apply(drop_arm_no_slash)
+            ][col7]
+        ),
         "Oil Tank": add_subtotal(df[df['Material'].apply(lambda x: matches_any(x, oil_tank_codes))][col7]),
         "All FG": add_subtotal(df[df['Material'].apply(not_761395513799_and_513899)][col7])
     }
@@ -115,6 +122,12 @@ if uploaded_file:
             df2000[
                 df2000['Material'].apply(lambda x: matches_any(x, vane_pump_codes))
                 & df2000['Material'].apply(not_761395513799_and_513899)
+            ][col7]
+        ),
+        "Drop Arm": add_subtotal(
+            df2000[
+                df2000['Material'].apply(lambda x: matches_any(x, drop_arm_codes))
+                & df2000['Material'].apply(drop_arm_no_slash)
             ][col7]
         ),
     }
